35617940390290fd49ab0c65b99d2dc964f48677
sanathNU <sanathn.upadhyaya@gmail.com>
2025-12-24 23:21:49 -0500

    fixed route error in svelte config

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/akshara-mantapa' : ''
+			base: process.env.NODE_ENV === 'production' ? '/Akshara-Mantapa' : ''
 		}
 	}
 };
